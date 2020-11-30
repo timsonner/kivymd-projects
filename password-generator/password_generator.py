37e5bd5497b0282cb3b4bd48a68ddc5bf66d287e
timsonner <66705347+timsonner@users.noreply.github.com>
2020-11-30 12:43:07 -0800

    Add files via upload
    Added the "required: True" attribute to the MDTextfield widget, this ensures that the on_release method will not process with no input, alleviating the need for an "if MDTextfield.text == "" :" check. Also, played around with the UI a bit, all in all I am happy with the app at this point.

--- password-generator/password_generator.py
+++ password-generator/password_generator.py
@@ -12,20 +12,22 @@ class MainApp(MDApp):
 
     def __init__(self, **kwargs):
         self.title = "Password Generator"
-        self.theme_cls.primary_palette = "Blue"
+        #self.theme_cls.primary_palette = "Blue"
         super().__init__(**kwargs)
 
     def build(self):
         return Builder.load_file("pass_gen_kivy.kv")    #load this kivy file
 
+
     def generate_password(self, n):
+        self.root.ids.password_output.text = ""
         ascii_for_pass = "".join(chr(c) for c in range(33, 127))    #range of ascii for the choice function
         pass_string = ""    #empty string
         while len(n.strip()) == 0:  #if MDtextfield is blank, return control
+            self.root.ids.text_field_input.error = True
             return
-        while n == "0":
-            self.root.ids.password_output.text = "\r"
-            return
+
+        self.root.ids.text_field_input.error = False
         n = int(n)  #must convert string to int
         for i in range(0, n):
             character = choice(ascii_for_pass)  #choose a character in range of ascii chars
